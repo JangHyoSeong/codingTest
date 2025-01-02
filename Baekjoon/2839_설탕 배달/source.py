db166e4586215ec81bacfa62f541ca7b530fa5d8
JHS <wkdgytmd200@naver.com>
2025-01-02 17:12:48 +0900

    [Baekjoon] 2839 설탕배달 Python3

---
+++ Baekjoon/2839_설탕 배달/source.py
@@ -0,0 +1,11 @@
+N = int(input())
+
+dp = [float('inf')] * (N + 1)
+dp[0] = 0 
+for i in range(3, N + 1):
+    if i >= 3:
+        dp[i] = min(dp[i], dp[i - 3] + 1)
+    if i >= 5:
+        dp[i] = min(dp[i], dp[i - 5] + 1)
+
+print(dp[N] if dp[N] != float('inf') else -1)
