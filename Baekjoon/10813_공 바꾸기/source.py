f98100e5a0e6f02d7dc12293e5d03c77d2ec8273
JHS <wkdgytmd200@naver.com>
2025-01-27 20:56:17 +0900

    [Baekjoon] 10813  공 바꾸기 Python3

---
+++ Baekjoon/10813_공 바꾸기/source.py
@@ -0,0 +1,8 @@
+N, M = map(int, input().split())
+
+arr = [i for i in range(1, N+1)]
+for _ in range(M):
+    a, b = map(int, input().split())
+    arr[a-1], arr[b-1] = arr[b-1], arr[a-1]
+
+print(" ".join(map(str, arr)))
