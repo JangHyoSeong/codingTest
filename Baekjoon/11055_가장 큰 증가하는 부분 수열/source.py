0e3452b78c962bfceafda6273a756c346cef4364
JHS <wkdgytmd200@naver.com>
2025-10-02 13:37:14 +0900

    [Baekjoon] 11055 가장 큰 증가하는 부분 수열 Python3

---
+++ Baekjoon/11055_가장 큰 증가하는 부분 수열/source.py
@@ -0,0 +1,11 @@
+N = int(input())
+arr = list(map(int, input().split()))
+
+dp = arr[:]
+
+for i in range(1, N):
+    for j in range(i):
+        if arr[j] < arr[i]:
+            dp[i] = max(dp[i], dp[j] + arr[i])
+
+print(max(dp))
